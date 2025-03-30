1ddc6a2906898c614d2038310d7f66b1a40f5909
Robert Jäschke <robert.jaeschke@hu-berlin.de>
2025-03-30 20:29:38 +0200

    cleaned up

--- rs500.py
+++ rs500.py
@@ -21,6 +21,11 @@ from html import unescape
 re_song = re.compile(r"<div class=\"performer\"><a href=\"poplist.*?>(.*?)</a></div><div><a href=\"poplist.*?>(.*)</a></div>")
 
 
+def get_html():
+   url = "https://poplist.de/poplist.php?site=1&var1=%20AND%20(tab_list_type.ID%20=%20959)&var4=Rolling%20Stone%20(2023)%20Die%20500%20besten%20Alben%20aller%20Zeiten%20-%20Kritiker&var5=Best-of-Lists%20from%20German%20Music%20Magazines%20-%20%20The%20500%20Best%20Albums%20of%20All%20Time&var10=Rolling%20Stone%20(2023)%20Die%20500%20besten%20Alben%20aller%20Zeiten,#list"
+   for i in range(1, 6):
+       yield get_page(url.replace("site=1", "site=" + str(i)), "rs_de_2023_" + str(i) + ".html")
+
 def get_page(url, fname):
     if os.path.isfile(fname):
         with open(fname, "rt") as f:
@@ -34,16 +39,16 @@ def get_page(url, fname):
 
 def parse_page(html):
     # <div class="performer"><a href="poplist.php?p=922#list">The Smiths</a></div><div><a href="poplist.php?t=1775#list">The Queen Is Dead</a></div>
-    for r in re_song.finditer(html):
-        artist = unescape(r.group(1).strip())
-        album = unescape(r.group(2).strip())
-        print(artist, album, sep=": ")
-
+    for h in html:
+        for r in re_song.finditer(h):
+            artist = unescape(r.group(1).strip())
+            album = unescape(r.group(2).strip())
+            yield (artist, album)
 
 
 if __name__ == '__main__':
-   url = "https://poplist.de/poplist.php?site=1&var1=%20AND%20(tab_list_type.ID%20=%20959)&var4=Rolling%20Stone%20(2023)%20Die%20500%20besten%20Alben%20aller%20Zeiten%20-%20Kritiker&var5=Best-of-Lists%20from%20German%20Music%20Magazines%20-%20%20The%20500%20Best%20Albums%20of%20All%20Time&var10=Rolling%20Stone%20(2023)%20Die%20500%20besten%20Alben%20aller%20Zeiten,#list"
 
-   for i in range(1, 6):
-       html = get_page(url.replace("site=1", "site=" + str(i)), "rs_de_2023_" + str(i) + ".html")
-       parse_page(html)
+    html = get_html()
+    albums = parse_page(html)
+    for i, (artist, album) in enumerate(albums):
+        print(str(i) + ". " + artist + ": " + album)
